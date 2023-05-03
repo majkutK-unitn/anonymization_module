a20173e0c59a96e48f15afb3bf9e1d1b5d3ab712
kmajkut_uni <kristof.majkut@studenti.unitn.it>
2023-05-03 10:13:06 +0200

    save the es connection as a class variable

--- db_connectors/ec_connector.py
+++ db_connectors/ec_connector.py
@@ -7,21 +7,19 @@ from os import getenv
 class EsConnector(MondrianAPI):
 
     def __init__(self):
-        self.API_KEY_ID = getenv('ES_API_KEY_ID')
-        self.API_KEY_SECRET = getenv('ES_API_KEY_SECRET')
-        self.ROOT_CA_PATH = getenv('ROOT_CA_PATH')
+        API_KEY_ID = getenv('ES_API_KEY_ID')
+        API_KEY_SECRET = getenv('ES_API_KEY_SECRET')
+        ROOT_CA_PATH = getenv('ROOT_CA_PATH')
 
-    def run_query(self, query):
-        print(self.ROOT_CA_PATH)
+        self.es = Elasticsearch(
+                hosts="https://neteye2.test:9200",
+                api_key=(API_KEY_ID, API_KEY_SECRET), 
+                ca_certs=ROOT_CA_PATH
+            )    
 
-        es = Elasticsearch(
-            hosts="https://neteye2.test:9200",
-            api_key=(self.API_KEY_ID, self.API_KEY_SECRET), 
-            ca_certs=self.ROOT_CA_PATH
-            )
-        
-        res = es.search(index="adult", query=query)
-        print("Got %d Hits:" % res['hits']['total']['value'])
+
+    def search(self, query):                
+        res = self.es.search(index="adults", query=query)                
         for hit in res['hits']['hits']:
             print("%(age)s %(native_country)s: %(education)s" % hit["_source"])            
 
