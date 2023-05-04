653a4d38b8b64bad366a8f8bfaf1b455237fe3c1
kmajkut_uni <kristof.majkut@studenti.unitn.it>
2023-05-04 09:55:52 +0200

    extend: read generalization hierarchies from anonymization config json

--- utils/read_gen_hierarchies.py
+++ utils/read_gen_hierarchies.py
@@ -42,3 +42,27 @@ def read_gen_hierarchy_file(treename: str) -> dict[str, GenTree]:
     tree_file.close()
 
     return root
+
+
+def read_gen_hierarchies_from_config(gen_hierarchies: list) -> dict[str, GenTree]:
+    """ Read the hierarchy tree from the gen_hierarchies attribute of config JSON file """
+
+    qid_dict: dict[str, GenTree] = {}
+
+    for hier in gen_hierarchies:                
+        root = read_child_nodes(hier["tree"], None)
+        qid_dict[hier["name"]] = root
+
+    return qid_dict
+
+
+def read_child_nodes(node, tree_parent: GenTree):
+    is_leaf = not bool(node["children"])        
+    tree_node = GenTree(node["value"], tree_parent, is_leaf)
+
+    if not is_leaf:
+        for child in node["children"]:
+            read_child_nodes(child, tree_node)
+
+    return tree_node
+
