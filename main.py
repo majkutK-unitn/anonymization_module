1bbbfaf10b3af9a7a4559da68bd97a94d176736c
kmajkut_uni <kristof.majkut@studenti.unitn.it>
2023-05-08 14:55:26 +0200

    feature: wire up the algorithm with the db connector

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
+import json
+import time
+
+from algorithms.mondrian.mondrian import Mondrian
+
 from db_connectors.ec_connector import EsConnector
 
 
-if __name__ == '__main__':
-    connector = EsConnector()
-    connector.search({"term": {"_id": {"value": "CTdo4IcBJ6zU8THOrfFz"}}})
-    connector.get_partition_count()
+def wire_up():
+    config_file = open('configs/adults_config.json')
+    config = json.load(config_file)
+
+    es_connector = EsConnector()
+    mondrian = Mondrian(es_connector, config)
+
+    config_file.close()
+
+    start_time = time.time()
+
+    mondrian.run()
+    
+    exec_time = float(time.time() - start_time)
+    ncp = mondrian.calculate_ncp()
+
+    print("K=", config["k"])
+    print("NCP %0.2f" % ncp + "%")
+    print("Run for %0.2f" % exec_time + " seconds")
+    
+
+if __name__ == '__main__':    
+    wire_up()
