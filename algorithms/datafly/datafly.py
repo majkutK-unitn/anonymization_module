e2cc4440ab39381a45d879827e44400bab6a6353
kmajkut_uni <kristof.majkut@studenti.unitn.it>
2023-05-15 10:46:09 +0200

    refactor: uniform num range and gen tree node processing

--- algorithms/datafly/datafly.py
+++ algorithms/datafly/datafly.py
@@ -40,6 +40,15 @@ class Datafly(AbstractAlgorithm):
         return new_partitions
     
 
+    def generate_new_partition_combinations(self, existing_partitions: list[dict[str, Attribute]], attr_name: str, nodes_or_ranges: list[GenTree | NumRange]):
+        new_partitions_accumulator: list[dict[str, Attribute]] = []
+
+        for node_or_range in nodes_or_ranges:
+            new_partitions_accumulator += self.combine_attribute_with_existing_partitions(existing_partitions, attr_name, node_or_range)
+
+        return new_partitions_accumulator
+
+        
     # (1) Get a dict of attributes: what generalization to use (num > how many buckets: cat > what level)
     # (2) Create an Attribute of each gen_value
     # (3) Iterate through all attributes and combine them one by one (age x edu_num: (age, edu_num) x race, (age, edu_num, race) x sex)
@@ -51,21 +60,13 @@ class Datafly(AbstractAlgorithm):
         for attr_name, value in self.numerical_attr_config.items():
             num_ranges = self.db_connector.spread_attribute_into_uniform_buckets(attr_name, value["datafly_num_of_buckets"])
 
-            new_partition_accumulator: list[dict[str, Attribute]] = []
-            for num_range in num_ranges:
-                new_partition_accumulator += self.combine_attribute_with_existing_partitions(temp_partitions, attr_name, num_range)
-
-            temp_partitions = new_partition_accumulator
+            temp_partitions = self.generate_new_partition_combinations(temp_partitions, attr_name, num_ranges)
                 
 
         for attr_name, value in self.categorical_attr_config.items():            
             nodes = self.gen_hiers[attr_name].nodes_on_level(value["datafly_init_level"])
 
-            new_partition_accumulator: list[dict[str, Attribute]] = []
-            for node in nodes:
-                new_partition_accumulator += self.combine_attribute_with_existing_partitions(temp_partitions, attr_name, node)
-
-            temp_partitions = new_partition_accumulator
+            temp_partitions = self.generate_new_partition_combinations(temp_partitions, attr_name, nodes)
 
         print("Done")
 
