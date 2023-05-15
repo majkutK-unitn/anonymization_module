2375f5c21dcc948356b01c1179df00660c1ddd0b
kmajkut_uni <kristof.majkut@studenti.unitn.it>
2023-05-15 10:41:31 +0200

    feature: implement initial partition generation

--- algorithms/datafly/datafly.py
+++ algorithms/datafly/datafly.py
@@ -1,7 +1,9 @@
 from interfaces.abstract_algorithm import AbstractAlgorithm
 from interfaces.datafly_api import DataflyAPI
+from models.attribute import Attribute
 
 from models.gentree import GenTree
+from models.numrange import NumRange
 from models.partition import Partition
 
 from utils.read_gen_hierarchies import read_gen_hierarchies_from_config_v2
@@ -25,16 +27,51 @@ class Datafly(AbstractAlgorithm):
                 self.numerical_attr_config[key] = value
 
         self.gen_hiers: dict[str, GenTree] = read_gen_hierarchies_from_config_v2(self.categorical_attr_config)
-        
+
+
+    def combine_attribute_with_existing_partitions(self, existing_partitions: list[dict[str, Attribute]], attr_name: str, range_or_node: GenTree | NumRange):
+        new_partitions: list[dict[str, Attribute]] = []
+
+        for partition_existing in existing_partitions:
+            partition_new = partition_existing.copy()
+            partition_new[attr_name] = Attribute(len(range_or_node), range_or_node.value)
+            new_partitions.append(partition_new)
+
+        return new_partitions
+    
 
     # (1) Get a dict of attributes: what generalization to use (num > how many buckets: cat > what level)
     # (2) Create an Attribute of each gen_value
     # (3) Iterate through all attributes and combine them one by one (age x edu_num: (age, edu_num) x race, (age, edu_num, race) x sex)
-    def generate_initial_partitions():
-        pass
+    #
+    # TODO: fix - check if multiple percentiles return the same value, and avoid creating ranges of (9,9)
+    def generate_initial_partitions(self):
+        temp_partitions: list[dict[str, Attribute]] = [{}]
+
+        for attr_name, value in self.numerical_attr_config.items():
+            num_ranges = self.db_connector.spread_attribute_into_uniform_buckets(attr_name, value["datafly_num_of_buckets"])
+
+            new_partition_accumulator: list[dict[str, Attribute]] = []
+            for num_range in num_ranges:
+                new_partition_accumulator += self.combine_attribute_with_existing_partitions(temp_partitions, attr_name, num_range)
+
+            temp_partitions = new_partition_accumulator
+                
+
+        for attr_name, value in self.categorical_attr_config.items():            
+            nodes = self.gen_hiers[attr_name].nodes_on_level(value["datafly_init_level"])
+
+            new_partition_accumulator: list[dict[str, Attribute]] = []
+            for node in nodes:
+                new_partition_accumulator += self.combine_attribute_with_existing_partitions(temp_partitions, attr_name, node)
+
+            temp_partitions = new_partition_accumulator
+
+        print("Done")
+
     
     def run(self) -> bool:
-        pass
+        self.generate_initial_partitions()
     
     def calculate_ncp(self) -> float:
         pass
