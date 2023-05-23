1fed2d7eb7d5c338611ffe1c9a1a50fc8465f3be
kmajkut_uni <kristof.majkut@studenti.unitn.it>
2023-05-23 11:37:23 +0200

    feature: read command line arguments in main for config, algorithm and backend

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ from interfaces.abstract_api import AbstractAPI
 from db_connectors.es_connector import EsConnector
 from db_connectors.mysql_connector import MySQLConnector
 
+import argparse
+
+parser = argparse.ArgumentParser('Anonymization Module')
+parser.add_argument('--algorithm', type=str, default='mondrian',
+                    help="K-Anonymity algorithm: mondrian / datafly (default: mondrian)")
+parser.add_argument('--backend', type=str, default='es',
+                    help="Backend to use: es / mysql (default: es)")
+parser.add_argument('--config', type=str, default='adults_config.json',
+                    help="Name of the config file: str (default: adults_config.json)")
+
 
 def read_config(file_name: str) -> dict[str, int|dict]:
     config_file = open(file_name)
@@ -19,35 +29,31 @@ def read_config(file_name: str) -> dict[str, int|dict]:
     return config
         
 
-def wire_up(algorithm: str, db_type: str) -> AbstractAlgorithm:
-    assert algorithm_name in ["datafly", "mondrian"]
+def wire_up(algorithm_name: str, db_type: str) -> AbstractAlgorithm:
+    assert algorithm_name in ["Datafly", "Mondrian"]
     assert db_type in ["Elasticsearch", "MySQL"]
 
     db_connector: AbstractAPI = EsConnector() if db_type == "Elasticsearch" else MySQLConnector()
 
-    if algorithm == "datafly":
+    if algorithm_name == "Datafly":
         return Datafly(db_connector)        
 
-    if algorithm == "mondrian":
-        return Mondrian(db_connector)
-    
-    raise Exception(f"No algorithm implementation name '{algorithm}' exists!")
+    if algorithm_name == "Mondrian":
+        return Mondrian(db_connector)        
 
 
-if __name__ == '__main__':
-    config_file_path = "configs/adults_config.json"
-    
-    # db_type = "Elasticsearch"
-    db_type = "MySQL"
-    algorithm_name = "datafly"
-    # algorithm_name = "mondrian"
-    algorithm = wire_up(algorithm_name, db_type)
+def main(args: dict):
+    config_file_path = f"configs/{args.config}"
+    db_backend = "Elasticsearch" if args.backend == "es" else "MySQL"
+    algorithm_name = "Mondrian" if args.algorithm == "mondrian" else "Datafly"
+
+    algorithm = wire_up(algorithm_name, db_backend)
 
     config = read_config(config_file_path)
 
     start_time = time.time()
 
-    print(f"Running {algorithm_name} on {db_type} backend with k={config['k']}...")
+    print(f"Running {algorithm_name} on {db_backend} backend with k={config['k']}...")
 
     algorithm.run(config)
     
@@ -57,3 +63,8 @@ if __name__ == '__main__':
         
     print("NCP %0.2f" % ncp + "%")
     print("Run for %0.2f" % exec_time + " seconds")
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
